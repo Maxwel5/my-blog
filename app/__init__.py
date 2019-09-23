8e5b13230a9371e8efa2b3f329d0388f4bbf10a6
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 14:48:28 +0300

    This imports login manager class from flas login extension and create class instance

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app(config_name):
     #initialising blueprints
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
