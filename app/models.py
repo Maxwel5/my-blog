ea9e4f98a1eb753067301782c10db22dc07ee9b2
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 13:54:36 +0300

    This imports database

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from . import db
+
+class User(db.Model):
+    __tablename__="users"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50)
+    blog = db.Column(db.String(120)
+    author = db.Column(db.String(80) 
+    
