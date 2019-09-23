07856ef35d72fde889b6d920ac3aff9d2246fd7d
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 14:45:11 +0300

    This installs migration and flask ligin

--- manage.py
+++ manage.py
@@ -5,11 +5,10 @@ from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
-
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
