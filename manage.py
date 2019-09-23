5c98ddf9b5f4a4134d068337e3296eb12235d91e
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 17:54:44 +0300

    This adds a user class

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
 from app.models import Blog
 from  flask_migrate import Migrate, MigrateCommand
 
@@ -12,7 +13,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, Blog=Blog)
+    return dict(app=app, db=db, Blog=Blog, User = User)
 
 if __name__ == '__main__':
     manager.run()
