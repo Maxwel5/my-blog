b2fdfe0176f00a555fd3d74a0c470f1cfa8b1cef
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 14:29:22 +0300

    This install flask migrate and import migrate and the migrate command class

--- app/models.py
+++ app/models.py
@@ -4,9 +4,9 @@ class User(db.Model):
     __tablename__="users"
     
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50)
-    blog = db.Column(db.String(120)
-    author = db.Column(db.String(80)
+    title = db.Column(db.String(50))
+    blog = db.Column(db.String(120))
+    author = db.Column(db.String(80))
 
     def __repr__(self):
         return f'User {self.title, blog, author}'  
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import user
+from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
@@ -10,7 +11,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User=User)
 
 if __name__ == '__main__':
     manager.run()
