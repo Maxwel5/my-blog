efcb96d38aafd7cbfbf055cb646fb05bf8f437b0
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 12:21:59 +0300

    This creates the ap,p configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
+bootsrap = Bootstrap()
+
+# Creating the app configurations
+app.config.from_object(config_options[config_name])
+
 def create_app(config_name):
     app = Flask(__name__)
 
