de346e4efb4720b67ee90819ba7c1e74752ac925
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 13:05:35 +0300

    This calls init app method and pass in the app instance

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,23 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
-
-
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
 
-    
-
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    #initialising blueprints
+    bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    #initialising blueprints
-    bootstrap.init_app(app)
+    
 
   
 
