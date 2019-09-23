5cc4748ba4597e0768030a03722ed24728e564a3
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 15:45:54 +0300

    This creates inputs

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField,TextAreaField
 from wtforms.validators import Required
 from ..models import User
+from wtforms import ValidationError
+
+class NewblogForm(FlaskForm):
+    title = StringField('Title', [validators.DataRequired()])
+    blog = TextAreaField('Blog', [validators.DataRequired()])
+    author = StringField('Author', [validators.DataRequired()])
+
+    
