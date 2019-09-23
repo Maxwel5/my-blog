354e6445b116a6cdbc6ba49b8fe721a769a4c929
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-23 13:55:32 +0300

    This connects our class to our database and allow communication

--- app/models.py
+++ app/models.py
@@ -6,5 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50)
     blog = db.Column(db.String(120)
-    author = db.Column(db.String(80) 
+    author = db.Column(db.String(80)
+
+    def __repr__(self):
+        return f'User {self.title, blog, author}'  
     
