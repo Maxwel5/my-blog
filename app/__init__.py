7ae536766aab0ecb6f8acebeb07ecd55f2063fe1
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-25 05:15:06 +0300

    This adds email account

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager,login_manager,login_user
 
 app =  Flask(__name__)
 bootstrap = Bootstrap()
--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+export MAIL_USERNAME=maxwellwafulasim@gmail.com
+export MAIL_PASSWORD=29ma83sw
 python3.6 manage.py server
